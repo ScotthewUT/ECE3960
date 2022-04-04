4abd9b5caa0236e317fab5f6381d91c6d7222883
ScotthewUT <42588153+ScotthewUT@users.noreply.github.com>
2022-04-04 15:50:24 -0600

    Updated PID line following test

--- BotTest.m
+++ BotTest.m
@@ -1,82 +1,21 @@
-%% CALIBRATE REFLECTANCE SENSOR
+%% INITIALIZE
+clear; clc; close all; instrreset;
+pause(0.1);
+%%
 IR_RR = 1;
 IR_CR = 2;
 IR_CL = 3;
 IR_LL = 4;
 MTR_R = 3;
 MTR_L = 4;
+MTR_MIN_SPD = 14;
 
 r = MKR_MotorCarrier;
-pause(0.02)
-r.reflectanceSetup();
-pause(0.02);
-data = zeros(50, 4);
-avg_data = zeros(2, 4);
+pause(0.1);
 
 %% CALIBRATE IR SENSORS
 
-fprintf("Place TBD Bot on light, path-free area.\n");
-fprintf("Calibrating reflectance sensors...\n");
-pause(0.5);
-for count = 0:4
-    r.setRGB(225,45,0);
-    fprintf("%i\n",5 - count);
-    pause(0.7);
-    r.setRGB(0,0,0);
-    pause(0.3);
-end
-r.setRGB(255,0,0);
-
-r.motor(MTR_R, 20);
-r.motor(MTR_L, -20);
-for row = 1:25
-    ref = r.readReflectance();
-    data(row,:) = ref(1,:);
-    pause(0.1);
-end
-r.motor(MTR_R, 0);
-r.motor(MTR_L, 0);
-pause(0.5);
-r.motor(MTR_R, -20);
-r.motor(MTR_L, 20);
-for row = 26:50
-    ref = r.readReflectance();
-    data(row,:) = ref(1,:);
-    pause(0.1);
-end
-r.motor(MTR_R, 0);
-r.motor(MTR_L, 0);
-r.setRGB(0,255,0);
-avg_data(1,:) = mean(data);
-avg_data(1,:) = round(avg_data(1,:));
-fprintf("  LL |  CL |  CR |  RR\n");
-fprintf(" %i | %i | %i | %i\n", avg_data(1,IR_LL), avg_data(1, IR_CL), ...
-        avg_data(1, IR_CR), avg_data(1, IR_RR));
-    
-fprintf("Place TBD Bot centered on black line path.\n");
-fprintf("Calibrating reflectance sensors...\n");
-pause(0.5);
-for count = 0:4
-    r.setRGB(225,45,0);
-    fprintf("%i\n",5 - count);
-    pause(0.7);
-    r.setRGB(0,0,0);
-    pause(0.3);
-end
-r.setRGB(255,0,0);
-
-data = zeros(25,4);
-for row = 1:25
-    ref = r.readReflectance();
-    data(row,:) = ref(1,:);
-    pause(0.15);
-end
-
-avg_data(2,:) = mean(data);
-avg_data(2,:) = round(avg_data(2,:));
-fprintf("  LL |  CL |  CR |  RR\n");
-fprintf(" %i | %i | %i | %i\n", avg_data(2, IR_LL),avg_data(2, IR_CL), ...
-        avg_data(2, IR_CR), avg_data(2, IR_RR));
+ref_cal_mat = ReflectanceCalibrate();
     
 %% NAIVE LINE FOLLOWING
 r.motor(MTR_R, MTR_MIN_SPD);
@@ -99,13 +38,16 @@ while true
     end
 end
 
-%% PID LINE FOLLOWING ?
-% r.motor(MTR_R, MTR_MIN_SPD);
-% r.motor(MTR_L, MTR_MIN_SPD);
-error = 0;
-kp = 0.2;
+%% PID LINE FOLLOWING
+MTR_SPD = 12;
+kp = 3;
 ki = 0;
 kd = 0;
+
+error = 0;
+error_sum = 0;
+error_delta = 0;
+
 while true
     ref = r.readReflectance();
     for idx = 1:length(ref)
@@ -114,7 +56,14 @@ while true
     prev_err = error;
     error = -2 * ref(IR_RR) - ref(IR_CR) + ref(IR_CL) + 2 * ref(IR_LL);
     disp(error)
-    pause(0.5);
+    %pause(0.5);
+    
+    mtr_r_ctrl = (kp * error) + (ki * error_sum) + (kd * error_delta) + MTR_SPD;
+    mtr_l_ctrl = (kp * -error) + (ki * error_sum) + (kd * error_delta) + MTR_SPD;
+    mtr_r_ctrl = round(mtr_r_ctrl);
+    mtr_l_ctrl = round(mtr_l_ctrl);
+    r.motor(MTR_R, mtr_r_ctrl);
+    r.motor(MTR_L, mtr_l_ctrl);
 end
 
 
