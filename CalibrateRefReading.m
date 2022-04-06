a71b19121dc1a3ab9243b329c5c1896fddb373a5
ScotthewUT <42588153+ScotthewUT@users.noreply.github.com>
2022-04-06 12:33:57 -0600

    New CalibrateRefReading function
    Small function to adjust IR reflectance readings with calibration data.

---
+++ CalibrateRefReading.m
@@ -0,0 +1,15 @@
+%% CALIBRATE IR REFLECTANCE READINGS
+function [adj_rdng] = CalibrateRefReading(readings, cal_data)
+
+IR_RR = 1;
+IR_CR = 2;
+IR_CL = 3;
+IR_LL = 4;
+
+adj_rdng = zeros(1,4);
+
+for idx = 1:length(adj_rdng)
+    adj_rdng(idx) = (readings(idx) - avg_data(1,idx)) / (avg_data(2,idx) - avg_data(1,idx));
+end
+
+end
