d611421d33ed712357f544f4a653c16e9aabda93
wallace1209 <86073132+wallace1209@users.noreply.github.com>
2022-04-13 14:59:33 -0600

    Add files via upload

--- MainLoop.m
+++ MainLoop.m
@@ -2,7 +2,6 @@
 clear; clc; close all; instrreset;
 pause(0.1);
 bot = MKR_MotorCarrier;
-r = MKR_MotorCarrier;
 pause(0.1);
 
 % DEFINE CONSTANTS
@@ -24,27 +23,58 @@ while true
             ir_cal_data = ReflectanceCalibrate(bot);
             bot_state = "READY";
         case "READY"       % Calibrated and placed in starting position.
-        case "FOLLOW"      % Following black line until intersection detected.
+        case "LINE_FOLLOW"      % Following black line until intersection detected.
             intersection_detected = FollowLine(bot, ir_cal_data);
             if intersection_detected
                 bot_state = "PICK PATH";
             end
         case "PICK PATH"   % At intersection and determing path choice.
+            %double checking if bot is grabbing the object or not
+            if grab_object== true %classification is good
+                %pick the "GOOD" path to drop location
+                bot_state= "LINE_FOLLOW";
+            else
+                
+                bot_state=""
+            end
+        case "PATH_FOLLOW"
+            intersection_detected = FollowLine(bot, ir_cal_data);
+            if grab_object==true
+                bot_state="DROP";
+            else
+                if intersection_detected
+                 bot_state="CHECK";
+                else 
+                 bot_state="RETREAT";
+            end
+            end
         case "CHECK"       % At path end; checking for pillar.
-            detect_pillar=Pilalr_Detection(r)
-            if detect_pillar==true
+            detect_pillar = Pillar_Detection(r)
+            if detect_pillar == true
                 bot_state = "APPROACH";
-            else 
-                bot_state = "RETREAT"
+            else
+                bot_state = "RETREAT";
+            end
         case "APPROACH"    % Approaching pillar.
-            approach_pillar=Approach_Pillar(r)
-            bot_state="GRAB"
+            approach_pillar = Approach_Pillar(r)
+            bot_state="GRAB";
         case "GRAB"        % Grabbing target with servo gripper.
             [encoder_val]=GrabBlock(bot)
+            grab_object = true;
             bot_state="CLASSIFY";
         case "RETREAT"     % Retreating from pillar and returning to line.
+            %function for retreating the bot
+            bot_state="PATH_FOLLOW";
         case "CLASSIFY"    % Classifying the currently held object.
+            
+            bot_state="RETREAT";
         case "DROP"        % Dropping object in goal.
+            intersection_detected = FollowLine(bot, ir_cal_data);
+            if intersection_detected
+            grab_object=false;
+            r.servo(2,0);
+            end
+            bot_state="RETREAT";
         otherwise          % Should never be reached...
     end
 end
