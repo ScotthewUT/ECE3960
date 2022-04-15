0556b96427a223d459627e8e9c64f3d2ece2d874
Procarioner <85374995+Procarioner@users.noreply.github.com>
2022-04-15 13:21:44 -0600

    Updated data_gather code 4/15

--- data_gather.m
+++ data_gather.m
@@ -1,20 +1,18 @@
 
-function [data] = data_gather(r)
+function [data] = data_gather(r, Label)
+r.startStream('analog');
 r.servo(2,180)
 pause(1)
 disp("Hold the object close to sensor for 5 seconds to calibrate")
 hall_data = r.analogRead(2);
-[r,g,b] = r.rgbRead();
+[red,g,b] = r.rgbRead();
 claw_data = r.analogRead(1);
-data = [red,g,b,hall_data, claw_data];
+data = [red,g,b,hall_data, claw_data, Label];
 for i=1:49
     hall_data=r.analogRead(2);
-    [r,g,b] = r.rgbRead();
+    [red,g,b] = r.rgbRead();
     claw_data = r.analogRead(1);
-    temp = [red,g,b,hall_data, claw_data];
-    data = [data; temp]
-pause(0.1)
+    temp = [red,g,b,hall_data, claw_data, Label];
+    data = [data; temp];
 end
 end
-
-%r.rgbRead()
