8d175443f14b6e1d816c02fb29de66791cf9c5a2
wallace1209 <86073132+wallace1209@users.noreply.github.com>
2022-04-18 13:42:16 -0600

    Add files via upload

--- drop_object.m
+++ drop_object.m
@@ -1,20 +1,5 @@
-function grab_object= drop_object(bot, classification)
-    intersection_detected = FollowLine(bot, ir_cal_data);
-    if intersection_detected
-        if classification == 1
-            bot.motor(MKR_R, 2)
-            bot.motor(MKR_L, 10);
-            FollowLine(bot, ir_cal_data);
-            r.servo(2,0);
-        elseif classification == 2
-            bot.motor(MKR_R, 10)
-            bot.motor(MKR_L, 2)
-            FollowLine(bot, ir_cal_data);
-            r.servo(2,0);
-        else
-            disp("I can't handle an excellent block yet");
-        end
-        
-    end
+function grab_object= drop_object(bot)
+    ApproachPillar(bot)
+    r.servo(2, 180)
     grab_object=false;
 end
